41a2bc8445c0ae98e5d6f1d8bc8a1ffccf6f9c85
Collin <collin@Zipcoders-MacBook-Pro.local>
2022-11-09 13:21:47 -0500

    getting some tests to pass but working out bugs in my code

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,90 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+    boolean result = false;
+    private Character [][] board;
+    public Board(Character[][] matrix) { board = matrix; }
+    public boolean isWinHorizontal(char letter){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (board[i][j] != letter){
+                    break;
+                }
+                else if (i == board.length - 1){
+                    return result = true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isWinVertical(char letter){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (board[i][j] != letter){
+                    break;
+                }
+                else if (j == board.length - 1){
+                    return result = true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isWinDiagonal(char letter){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (i == j && i == letter && j == letter){
+                    return result = true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isWinOppositeDiagonal(char letter){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if ((i + j == board.length - 1) && i == letter && j == letter){
+                    return result = true;
+                }
+            }
+        }
+        return false;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        char letter = 'X';
+        if ((isWinHorizontal(letter) || isWinVertical(letter) || isWinDiagonal(letter) || isWinOppositeDiagonal(letter))){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        char letter = 'O';
+        if ((isWinHorizontal(letter) || isWinVertical(letter) || isWinDiagonal(letter) || isWinOppositeDiagonal(letter))){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if (!isInFavorOfO() && !isInFavorOfX()){
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfO()){
+            return "O";
+        }
+        else if (isInFavorOfX()){
+            return "X";
+        } else
+        return "";
     }
 
 }
